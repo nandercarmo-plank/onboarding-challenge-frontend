626e0c35f05ed286b18cadae78a83a63be962d32
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-06-06 15:44:27 -0300

    Add deployment pipe.

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+resource "vercel_project" "frontend_vercel_deploy" {
+  name      = "onboarding-challenge-frontend"
+  git_repository = {
+    type = "github"
+    repo = "nandercarmo-plank/onboarding-challenge-frontend"
+  }
+}
