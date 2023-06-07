3d87c9a7728b83ff561f6ac96535ae3a480bbec9
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-06-07 15:05:06 -0300

    Update actions: add vercel deployment.

--- cypress/e2e/NotFoundUserFlow.cy.ts
+++ cypress/e2e/NotFoundUserFlow.cy.ts
@@ -1,19 +1,6 @@
 import '../support/e2e';
 
-const createdCrew = {
-	name: "E2E Test Crew",
-	crewmans: "2, 3, 4"
-};
-
-const updatedCrew = {
-	name: "E2E Test Crew - Updated",
-	crewmans: "2, 4"
-};
-
-describe("Crew user flow", () => {
-
-	beforeEach(() => {
-	});
+describe("Not found page flow", () => {
 
 	it("Should open the not found page", () => {
 		cy.visit("/test");
